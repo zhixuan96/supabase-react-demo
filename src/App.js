5731edd5e97702bceb0b408bffb032d5adfd1119
zhixuan du <zhixuan.du@unity3d.com>
2022-06-28 14:27:19 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+import supabase from './supabase-client'
+import React from 'react';
+function App() {
+
+  React.useEffect(()=>{
+    supabase.from("local_table_demo").select().then(console.log);
+  },[])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
