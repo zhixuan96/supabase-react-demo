76982a245fb5285fca31c15775e40cec74728a4a
zhixuandu <zhixuan.du@unity3d.com>
2022-07-13 13:47:50 +0800

    update public link

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -1,6 +1,6 @@
 import{createClient} from '@supabase/supabase-js';
 
-let supabaseUrl = "http://localhost:8000";
+let supabaseUrl = "http://101.43.66.175:8000";
 let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlLWRlbW8iLCJpYXQiOjE2NDE3NjkyMDAsImV4cCI6MTc5OTUzNTYwMH0.q58NgLHsiOb9lfhr4g3suTU0h8t16SbkANR3iQIagaE";
 
 export const supabase = createClient(supabaseUrl,supabaseKey);
